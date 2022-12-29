c50749e0d9b52b51165c083f2adb7abf2ebf3195
Jared Mullins <jared.mullins@coxautoinc.com>
2022-12-28 18:13:57 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+require('dotenv').config();
+
+const UserModel = require("./models/User")
+
+
+app.use(express.json())
+
+mongoose.connect(`mongodb+srv://my-user:${process.env.MONGO_PASSWORD}@cluster0.itbdo5l.mongodb.net/drone-drop?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ messgae: err.messgae })
+    }
+})
+
+app.get('/user/:username', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ username: req.params.username });
+        console.log(user)
+        res.json(user)
+    } catch (err) {
+        res.status(500).json({ messgae: err.messgae })
+    }
+})
+
+app.post('/user', async (req, res) => {
+    console.log(req.body)
+    const user = new UserModel(req.body)
+    try {
+        await user.save();
+        res.send("inserted data")
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+
+app.listen(3001, () => {
+    console.log('Server running on port 3001...')
+})
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    },
+    latitude: {
+        type: Number,
+        required: true
+    }
+})
+
+const User = mongoose.model("drones", UserSchema)
+
+module.exports = User;
