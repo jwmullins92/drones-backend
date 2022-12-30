42e3ed72f60ed397e6328043d7b865c502ae8153
Jared Mullins <jared.mullins@coxautoinc.com>
2022-12-29 19:18:52 -0600

    test

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ const UserModel = require("./models/User")
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(`mongodb+srv://my-user:${process.env.MONGO_PASSWORD}@cluster0.itbdo5l.mongodb.net/drone-drop?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://my-user:2aZiI6fXkCHzRkPt@cluster0.itbdo5l.mongodb.net/drone-drop?retryWrites=true&w=majority`, {
     useNewUrlParser: true
 })
 const db = mongoose.connection
 db.once('connected', () => {
     console.log('Database Connected');
+})
+
+app.get('/', async (req, res) => {
+    res.send("I'M WORKING")
 })
 
 app.get('/users', async (req, res) => {
@@ -48,8 +52,17 @@ app.post('/user', async (req, res) => {
     }
 })
 
+app.delete('/user/:username', async (req, res) => {
+    try {
+        const user = await UserModel.findOneAndDelete({ username: req.params.username })
+        res.json(user)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 
-app.listen(3001, () => {
+
+app.listen(process.env.PORT || 8080, () => {
     console.log('Server running on port 3001...')
 })
